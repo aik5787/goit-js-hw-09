1b5a2487b435e41b37202b057f970a76ef55ef26
aik5787 <127675179+aik5787@users.noreply.github.com>
2023-07-21 19:02:30 +0000

    Deploying to gh-pages from @ aik5787/goit-js-hw-09@0691d4f864933fc9d51b7736dd9fae847e225168 🚀

---
+++ 01-color-switcher.de0767d9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));let d=null;
+//# sourceMappingURL=01-color-switcher.de0767d9.js.map
